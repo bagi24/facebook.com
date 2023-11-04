adbe1db7941aefe93a030ba7411c553a470e8141
unknown <bagrat.injgia24@gmail.com>
2023-11-04 22:22:48 +0400

    install icon-material and next began working on the  Navbar component

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ import LeftBar from "./components/leftBar/LeftBar";
 import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+import { Navigate } from "react-router-dom";
 
 function App() {
+  const currentUser = true;
+
   const Layout = () => {
     return (
       <div>
@@ -23,10 +26,22 @@ function App() {
     );
   };
 
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
       children: [
         {
           path: "/",
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,44 @@
 import "./navbar.scss";
+import {
+  HomeOutlined,
+  DarkModeOutlined,
+  EmailOutlined,
+  PersonOutlined,
+  SearchOutlined,
+  GridViewOutlined,
+  NotificationsOutlined,
+} from "@mui/icons-material/";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>Facebook</span>
+        </Link>
+        <HomeOutlined />
+        <DarkModeOutlined />
+        <GridViewOutlined />
+        <div className="search">
+          <SearchOutlined />
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlined />
+        <EmailOutlined />
+        <NotificationsOutlined />
+        <div className="user">
+          {/* <img
+            src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          /> */}
+          <span>Bagrat injgia</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
