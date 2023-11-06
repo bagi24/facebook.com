d0813b6ba5d54793695d502b45900d23cb7b8002
unknown <bagrat.injgia24@gmail.com>
2023-11-06 14:11:48 +0400

    create style.scss file in src and write Sass-Themify code for all components

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import { Navigate } from "react-router-dom";
+import "./style.scss";
 
 function App() {
   const currentUser = true;
 
   const Layout = () => {
     return (
-      <div>
+      <div className="theme-dark">
         <Navbar />
 
         <div style={{ display: "flex" }}>
